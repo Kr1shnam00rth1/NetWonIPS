cecb20f2acf83bbe45ed2d889dc4c1decbafccda
Kr1shnam00rth1 <plkrishnamoorthi2004@gmail.com>
2024-11-21 14:06:33 +0530

    ssh completed

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import threading
 import processSSH
 
-t1=threading.Thread(target=processSSH.MonitorSSHLogs)
-t1.start()
+def display_banner():
+    banner = r"""
+  ███╗   ██╗███████╗████████╗██╗    ██╗ ██████╗ ███╗   ██╗
+  ████╗  ██║██╔════╝╚══██╔══╝██║    ██║██╔═══██╗████╗  ██║
+  ██╔██╗ ██║█████╗     ██║   ██║ █╗ ██║██║   ██║██╔██╗ ██║
+  ██║╚██╗██║██╔══╝     ██║   ██║███╗██║██║   ██║██║╚██╗██║
+  ██║ ╚████║███████╗   ██║   ╚███╔███╔╝╚██████╔╝██║ ╚████║
+  ╚═╝  ╚═══╝╚══════╝   ╚═╝    ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═══╝
+
+          NetWon Intrusion Prevention System (IPS)
+         ------------------------------------------
+            Developed by: Krishnamoorthi P L
+            Version: 1.0
+            Description :
+         ------------------------------------------
+          Because in security, the net always wins!
+"""
+    print(banner)
+
+if __name__ == "__main__":
+    display_banner()
+    t1=threading.Thread(target=processSSH.MonitorSSHLogs)
+    t1.start()
--- processSSH.py
+++ processSSH.py
@@ -1,56 +1,55 @@
-"""
-    Modules to detect SSH Brute force
-"""
-
 import datetime
 import doActions
+import makeLogs
+import time
 
-ip_failed_count={}
-threshold_time=5*60 
-threshold_count=5
-
+ip_failed_count = {}
+threshold_time = 5 * 60
+threshold_count = 5
 
 def CheckTimeDifference(start_time, current_time):
-   """
-     Function to Check if the time difference between the first packet and the current packet is within the threshold.
-   """
+
+    """Function to Check if the time difference between the first packet and the current packet is within the threshold."""
+    
     start_time = datetime.datetime.strptime(start_time, '%H:%M:%S')
     current_time = datetime.datetime.strptime(current_time, '%H:%M:%S')
-    
+
     start_time = start_time.replace(tzinfo=datetime.timezone.utc)
     current_time = current_time.replace(tzinfo=datetime.timezone.utc)
-    
+
     difference_time = (current_time - start_time).total_seconds()
     return 1 if difference_time < threshold_time else 0
 
 def MonitorSSHLogs():
-    """
-       Function to Monitor the SSH logs to detect failed login attempts, and block IPs exceeding the threshold count within the threshold time.
-    """
-    file =open("/var/log/auth.log")
+
+    """Function to Monitor the SSH logs to detect failed login attempts, and block IPs exceeding the threshold count within the threshold time."""
+    
+    file = open("/var/log/auth.log")
+    file.seek(0,2)
     while True:
-        for line in reversed(file.readlines()):
-            line=line.strip().split(" ")
-            if 'Failed' in line and len(line)==14:
-                ip=line[10]
-                timestamp=line[2]
+        for line in file.readlines():
+            line = line.strip().split(" ")
+            if 'Failed' in line and len(line) == 14:
+                ip = line[10]
+                timestamp = line[2]
                 if ip not in ip_failed_count:
-                    ip_failed_count[ip]=[1,timestamp]
+                    ip_failed_count[ip] = [1, timestamp]
                 else:
-                    temporary=ip_failed_count[ip]
-                    if temporary[0]>threshold_count:
-                        current_time=datetime.datetime.now().strftime('%H:%M:%S')
-                        start_time=temporary[1]
-                        result=CheckTimeDifference(start_time,current_time)
-                
+                    temporary = ip_failed_count[ip]
+                    if temporary[0] > threshold_count:
+                        current_time = datetime.datetime.now().strftime('%H:%M:%S')
+                        start_time = temporary[1]
+                        result = CheckTimeDifference(start_time, current_time)
+
                         if result == 1:
                             doActions.BlockIP(ip)
+                            makeLogs.Attacklogs(f'SSH Bruteforce IP {ip} Blocked',None)
                             ip_failed_count.pop(ip)
+                            ip=None
                         else:
-                            ip_failed_count[ip]=[1,timestamp]
+                            ip_failed_count[ip] = [1, timestamp]
                     else:
-                        failed_count=ip_failed_count[ip][0]
-                        ip_failed_count[ip]=[failed_count+1,timestamp]
+                        failed_count = ip_failed_count[ip][0]
+                        ip_failed_count[ip] = [failed_count + 1, timestamp]
     file.close()
-
-
+0

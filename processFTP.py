e3dffab7465449d424f46bc5220a316858e0db1d
Kr1shnam00rth1 <plkrishnamoorthi2004@gmail.com>
2024-12-07 08:44:46 +0530

    Updated time logic

--- processFTP.py
+++ processFTP.py
@@ -15,15 +15,15 @@ def CheckTimeDifference(start_time, current_time):
 
     """Function to check if the time difference between the first packet and the current packet is within the threshold."""
     
-    start_time = datetime.datetime.strptime(start_time, '%H:%M:%S')
-    current_time = datetime.datetime.strptime(current_time, '%H:%M:%S')
-
-    start_time = start_time.replace(tzinfo=datetime.timezone.utc)
-    current_time = current_time.replace(tzinfo=datetime.timezone.utc)
-
-    difference_time = (current_time - start_time).total_seconds()
-    return 1 if difference_time < threshold_time else 0
-
+    s_hour, s_min, s_second = map(int, start_time.split(":"))
+    c_hour, c_min, c_second = map(int, current_time.split(":"))
+    start_time = s_hour * 3600 + s_min * 60 + s_second
+    current_time = c_hour * 3600 + c_min * 60 + c_second
+    difference_time = (current_time - start_time)
+    if difference_time < threshold_time:
+        return 1
+    return 0
+    
 def MonitorFTPLogs():
     """Function to monitor the FTP logs to detect failed login attempts and block IPs exceeding the threshold count within the threshold time."""
     

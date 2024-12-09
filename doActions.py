a2f1a022d7cc426bf888e630b6d156f8ab596427
Kr1shnam00rth1 <plkrishnamoorthi2004@gmail.com>
2024-12-10 02:17:24 +0530

    do actions updated

--- doActions.py
+++ doActions.py
@@ -3,7 +3,7 @@
 """
 import subprocess
 
-def IsBlocked(ip):
+def IsIncommingBlocked(ip):
     
     """ Function to check does IP already blocked or not"""
     
@@ -17,16 +17,46 @@ def IsBlocked(ip):
         print(f"Failed to check iptables rules. Error: {e}")
         return False
 
-def BlockIP(ip):
+def IsOutgoingBlocked(ip):
+    
+    """ Function to check does IP already blocked or not"""
+    
+    try:
+        
+        result = subprocess.run(['sudo', 'iptables', '-L', 'OUTPUT', '-v', '-n'], capture_output=True, text=True, check=True)
+        if ip in result.stdout:
+            return True
+        return False
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to check iptables rules. Error: {e}")
+        return False
+
+def IncommingIpBlock(ip):
 
     """ Function to bloack ip"""
     
-    if IsBlocked(ip):
+    if IsIncommingBlocked(ip):
         return 0
     else:
         try:
             subprocess.run(['sudo', 'iptables', '-A', 'INPUT', '-s', ip, '-j', 'DROP'], check=True)
-            print(f"IP address {ip} has been blocked.")
+            print(f"IP address {ip} has been blocked check logs.")
+            return 1
+            
+        except subprocess.CalledProcessError as e:
+            print(f"Failed to block IP address {ip}. Error: {e}")
+
+
+def OutgoingIpBlock(ip):
+
+    """ Function to bloack ip"""
+    
+    if IsOutgoingBlocked(ip):
+        return 0
+    else:
+        try:
+            subprocess.run(['sudo', 'iptables', '-A', 'OUTPUT', '-s', ip, '-j', 'DROP'], check=True)
+            print(f"IP address {ip} has been blocked check logs.")
             return 1
             
         except subprocess.CalledProcessError as e:
